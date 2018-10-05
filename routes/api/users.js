920ed82130ad28d6f0342ac7934e85206f77f244
Ramon Giron II <rg2@RG2.lan>
2018-10-05 16:35:05 -0400

    user mongo password req res

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const router = express.Router();
-
+const gravatar = require('gravatar');
+// load user model
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 //@route    GET api/users/test
 //@desc     Tests users route
 //@access   public
 
 router.get('/test', (req, res) => res.json({msg:"Users Works"}));
+
+
+//@route    GET api/users/register
+//@desc     Register user
+//@access   Public
+// .then is a promise
+router.post('/register', (req,res) => {
+User.findOne({ email: req.body.email })
+.then(user => {
+  if (user) {
+    return res.status(400).json({ email: 'email already exists' });
+  } else {
+    const avatar = gravatar.url(req.body.email, {
+      s:'200', //size
+      r:'pg',//rating
+      d:'mm' //default
+    });
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      avatar,
+      password: req.body.password
+    });
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+        newUser.password = hash;
+        newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+      })
+    })
+  }
+})
+
+});
+
 
 module.exports = router;
