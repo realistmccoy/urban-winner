4961336bef56407824d2be21b4a3e62f5fe55340
Ramon Giron II <rg2@RG2.lan>
2019-06-20 18:12:41 -0400

    redux done

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import {GET_ERRORS} from './types'
+import setAuthToken from '../utils/setAuthToken'
+import jwt_decode from 'jwt-decode'
+
+import {GET_ERRORS, SET_CURRENT_USER } from './types'
+
 
 //Register user
 
@@ -15,4 +19,36 @@ export const registerUser = (userData, history) => dispatch => {
             })
             );
 
+}
+
+//Login - Get user token
+
+export const LoginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+  .then(res => {
+    //save to local storage
+    const {token} = res.data
+    //set token localstorage
+    localStorage.setItem('jwtToken', token);
+    // Set token to Auth header
+    setAuthToken(token);
+    // decode token to get user data
+    const decoded = jwt_decode(token);
+    // set current user
+    dispatch(setCurrentUser(decoded))
+  })
+  .catch(err => dispatch({
+    type: GET_ERRORS,
+    payload: err.response.data
+  })
+  )
+}
+
+//set logged in user
+
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
