65d3f351f7eedc7b09c6f0015ba18a24a2054606
Ramon Giron II <rg2@RG2.lan>
2019-06-28 05:40:20 -0400

    exp and edu edit/delete/add finish

--- client/src/App.js
+++ client/src/App.js
@@ -18,9 +18,13 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile'
+import AddExperience from './components/add-credentials/AddExperience'
+import AddEducation from "./components/add-credentials/AddEducation";
 
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
+
 
 // check for token
 
@@ -69,6 +73,31 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+
+                   <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+
+                 <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+
+              
+                 <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
               
